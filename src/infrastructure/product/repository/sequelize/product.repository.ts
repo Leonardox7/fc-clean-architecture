0ab91636b4fc811118a8e9ebbe871ec5572de964
Leonardox7 <leonardoz5z@hotmail.com>
2024-01-09 07:54:49 -0300

    fix: Add custom exception when product not founded

--- src/infrastructure/product/repository/sequelize/product.repository.ts
+++ src/infrastructure/product/repository/sequelize/product.repository.ts
@@ -1,7 +1,7 @@
-import Product from "../../../../domain/product/entity/product";
-import ProductInterface from "../../../../domain/product/entity/product.interface";
-import ProductRepositoryInterface from "../../../../domain/product/repository/product-repository.interface";
-import ProductModel from "./product.model";
+import Product from '../../../../domain/product/entity/product';
+import ProductInterface from '../../../../domain/product/entity/product.interface';
+import ProductRepositoryInterface from '../../../../domain/product/repository/product-repository.interface';
+import ProductModel from './product.model';
 
 export default class ProductRepository implements ProductRepositoryInterface {
   async create(entity: ProductInterface): Promise<void> {
@@ -27,14 +27,28 @@ export default class ProductRepository implements ProductRepositoryInterface {
   }
 
   async find(id: string): Promise<Product> {
-    const productModel = await ProductModel.findOne({ where: { id } });
-    return new Product(productModel.id, productModel.name, productModel.price);
+    try {
+      const productModel = await ProductModel.findOne({
+        where: {
+          id,
+        },
+        rejectOnEmpty: true,
+      });
+      return new Product(
+        productModel.id,
+        productModel.name,
+        productModel.price
+      );
+    } catch (error) {
+      throw new Error('Product not found');
+    }
   }
 
   async findAll(): Promise<Product[]> {
     const productModels = await ProductModel.findAll();
-    return productModels.map((productModel) =>
-      new Product(productModel.id, productModel.name, productModel.price)
+    return productModels.map(
+      (productModel) =>
+        new Product(productModel.id, productModel.name, productModel.price)
     );
   }
 }
